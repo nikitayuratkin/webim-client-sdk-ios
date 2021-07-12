4588c0d89dfbd6e320c7c036506cd3e0fdd42005
nikitayuratkin <YuratkinNS@uralsib.ru>
2021-07-12 15:30:00 +0300

    sqlite dependency fixed

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     targets: [
         .target(name: "WebimClientLibrary",
-                dependencies: ["SQLite"],
+                dependencies: ["SQLite.swift"],
                 path: "Source/WebimClientLibrary")
     ]
 )
