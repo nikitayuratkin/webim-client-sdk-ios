d9aa32e6c33b2138f847a9c6f2f12c862629e3fb
nikitayuratkin <YuratkinNS@uralsib.ru>
2021-07-12 13:36:21 +0300

    123

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "WebimClientLibrary",
+    platforms: [
+       .iOS(.v9)
+    ],
+    products: [
+        .library(name: "WebimClientLibrary", targets: ["WebimClientLibrary"])
+    ],
+    dependencies: [
+        .package(url: "git@github.com:nikitayuratkin/SQLite.swift.git", .upToNextMinor(from: "0.12.0"))
+    ],
+    targets: [
+        .target(name: "WebimClientLibrary",
+                dependencies: ["SQLite"],
+                path: "Source/WebimClientLibrary")
+    ]
+)
